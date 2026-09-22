69e82c69db64665d49bc3187941a560f2918536d
CWP-knamsuph <67010056@kmitl.ac.th>
2026-09-22 23:48:41 +0700

    Add files via upload

--- cell04/ex02/calculator.py
+++ cell04/ex02/calculator.py
@@ -1,1 +1,8 @@
+num1 = int(input("Give me the first number: "))
+num2 = int(input("Give me the second number: "))
 
+print("Thank you!")
+print(num1, "+", num2, "=", num1 + num2)
+print(num1, "-", num2, "=", num1 - num2)
+print(num1, "/", num2, "=", num1 / num2)
+print(num1, "*", num2, "=", num1 * num2)
