91a0f19afa2fb23ee8f493b894b39441a5e406a3
CWP-knamsuph <67010056@kmitl.ac.th>
2026-09-22 14:43:30 +0700

    Add files via upload

--- cell02/ex01/isneg.py
+++ cell02/ex01/isneg.py
@@ -1,1 +1,7 @@
-
+number = int(input("Type Number "))
+if number < 0:
+    print("This number is Negative.")
+elif number > 0:
+    print("This number is Positive.")
+else:
+    print("This number is both positive and negative.")
