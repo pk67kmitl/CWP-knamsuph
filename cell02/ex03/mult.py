502fffaed603c948f82c13e227136e831bd98b6e
CWP-knamsuph <67010056@kmitl.ac.th>
2026-09-22 14:46:21 +0700

    Add files via upload

--- cell02/ex03/mult.py
+++ cell02/ex03/mult.py
@@ -1,1 +1,13 @@
+first_number = int(input("Enter the first number: "))
+second_number = int(input("Enter the second number: "))
 
+result = first_number * second_number
+
+print(first_number, "x", second_number, "=", result)
+
+if result > 0:
+    print("The result is positive.")
+elif result < 0:
+    print("The result is negative.")
+else:
+    print("The result is positive and negative.")
