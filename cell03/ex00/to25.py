47912f22c7d6d8f3bf7fab78dff3203979ad78bf
CWP-knamsuph <67010056@kmitl.ac.th>
2026-09-22 14:47:48 +0700

    Add files via upload

--- cell03/ex00/to25.py
+++ cell03/ex00/to25.py
@@ -1,1 +1,9 @@
+print("Enter a number less than 25 ")
+num = int(input())
 
+if num <= 25:
+    while num <= 25:
+        print("Inside the loop, my variable is", num)
+        num += 1
+else:
+    print("Error")
