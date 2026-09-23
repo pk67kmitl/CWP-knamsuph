363682f03c37c2b9d65941b11f5409bc24c8a480
CWP-knamsuph <67010056@kmitl.ac.th>
2026-09-23 22:57:50 +0700

    Add files via upload

--- cell05/ex10/parameter_matching.py
+++ cell05/ex10/parameter_matching.py
@@ -1,1 +1,9 @@
-
+import sys
+if len(sys.argv) == 2:
+    word = input("What was the parameter? ")
+    if word == sys.argv[1]:
+        print("Good job!")
+    else:
+        print("Nope, sorry...")
+else:
+    print("none")
