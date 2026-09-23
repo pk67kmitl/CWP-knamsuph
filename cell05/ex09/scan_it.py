56e140277a0f34a78dddc9b740fb084124b12f85
CWP-knamsuph <67010056@kmitl.ac.th>
2026-09-23 22:57:03 +0700

    Add files via upload

--- cell05/ex09/scan_it.py
+++ cell05/ex09/scan_it.py
@@ -1,1 +1,12 @@
-
+import sys
+import re
+if len(sys.argv) == 3:
+    keyword = sys.argv[1]
+    string = sys.argv[2]
+    result = re.findall(keyword, string)
+    if len(result) > 0:
+        print(len(result))
+    else:
+        print("none")
+else:
+    print("none")
