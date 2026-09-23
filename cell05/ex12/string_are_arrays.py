8be8c45496959c287b60a03c8bb38de6b052bc2f
CWP-knamsuph <67010056@kmitl.ac.th>
2026-09-23 22:59:32 +0700

    Add files via upload

--- cell05/ex12/string_are_arrays.py
+++ cell05/ex12/string_are_arrays.py
@@ -1,1 +1,14 @@
-
+import sys
+if len(sys.argv) == 2:
+    string = sys.argv[1]
+    count = 0
+    for char in string:
+        if char == "z":
+            print("z", end="")
+            count += 1
+    if count == 0:
+        print("none")
+    else:
+        print()
+else:
+    print("none")
